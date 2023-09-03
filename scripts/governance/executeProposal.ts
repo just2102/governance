645a18bb19bacb4764f84b3cdab078cc8023d025
just2102 <thesixteensaltines@gmail.com>
2023-09-03 14:23:39 +0700

    add fast-forward

--- scripts/governance/executeProposal.ts
+++ scripts/governance/executeProposal.ts
@@ -4,12 +4,14 @@ import { ethers } from "hardhat";
 
 export async function executeProposal(
   Governance: Governance,
+  proposalId: string,
   _to: string | Addressable,
   _value: bigint,
   _func: string,
   _data: string,
   _description: string
 ) {
+  await fastForward();
   const executeTx = await Governance.executeProposal(
     _to,
     _value,
@@ -20,4 +22,9 @@ export async function executeProposal(
   const executeReceipt = await executeTx.wait();
   console.log("executed successfully");
   return executeReceipt;
+}
+
+async function fastForward() {
+  await ethers.provider.send("evm_increaseTime", [86400 * 3]);
+  await ethers.provider.send("evm_mine"); // mine a new block
 }
